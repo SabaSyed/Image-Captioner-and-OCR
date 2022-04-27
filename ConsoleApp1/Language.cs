53f4c785448ff2783a2131dcec12e12aaf22af6e
SabaSyed <68774325+SabaSyed@users.noreply.github.com>
2022-04-27 07:06:08 +0500

    Translation via console app
    Console Translation C#

---
+++ ConsoleApp1/Language.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Language
+    {
+        public static void LanguageMap(Dictionary<string, string> language)
+
+        {
+            
+            language.Add("Afrikaans", "af");
+
+            language.Add("Albanian", "sq");
+
+            language.Add("Arabic", "ar");
+
+            language.Add("Armenian", "hy");
+
+            language.Add("Azerbaijani", "az");
+
+            language.Add("Danish", "da");
+
+            language.Add("Dutch", "nl");
+
+            language.Add("English", "en");
+
+        }
+    }
+}
