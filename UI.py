3f1f87e04c5baf92f365704ff1a8ec875a5d675c
SabaSyed <68774325+SabaSyed@users.noreply.github.com>
2022-05-06 15:33:54 +0500

    Simple Gui in Python
    Created GUI in python

---
+++ UI.py
@@ -0,0 +1,208 @@
+from tkinter import *
+import os
+import cv2
+import numpy as np
+import glob
+import random
+from PIL import ImageTk,Image
+from tkinter.filedialog import askopenfile
+from tkinter import filedialog
+
+
+# Designing window for registration
+ 
+def register():
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("300x250")
+ 
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+ 
+    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password * ")
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
+ 
+ 
+# Designing window for login 
+ 
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+ 
+    global username_verify
+    global password_verify
+ 
+    username_verify = StringVar()
+    password_verify = StringVar()
+ 
+    global username_login_entry
+    global password_login_entry
+ 
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
+ 
+# Implementing event on register button
+ 
+def register_user():
+ 
+    username_info = username.get()
+    password_info = password.get()
+ 
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+ 
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+ 
+    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+ 
+# Implementing event on login button 
+ 
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+ 
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+ 
+        else:
+            password_not_recognised()
+ 
+    else:
+        user_not_found()
+ 
+# Designing popup for login success
+ 
+def login_sucess():
+    login_screen.destroy()
+    main_screen.destroy()
+    global my_w 
+    my_w= Tk()
+    my_w.geometry("400x300")  # Size of the window 
+    my_w.title('www.plus2net.com')
+    my_font1=('times', 18, 'bold')
+    l1 = Label(my_w,text='Add Student Data with Photo',width=30,font=my_font1)  
+    l1.grid(row=1,column=1)
+    b1 = Button(my_w, text='Upload File', 
+    width=20,command = lambda:upload_file())
+    b1.grid(row=2,column=1) 
+
+ 
+def upload_file():
+    global img
+    f_types = [('Jpg Files', '*.jpg')]
+    filename = filedialog.askopenfilename(filetypes=f_types)
+    # img = ImageTk.PhotoImage(file=filename)
+    # img_resized=img.resize((400,200)) # new width & height
+    # img=ImageTk.PhotoImage(img_resized)
+
+    img=Image.open(filename)
+    width, height = img.size
+    width_new=int(width/3)
+    height_new=int(height/3)
+    img_resized=img.resize((width_new,height_new))
+
+    b2 =Button(my_w,image=img) # using Button 
+    b2.grid(row=3,column=1)
+
+# Designing popup for login invalid password
+ 
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+ 
+# Designing popup for user not found
+ 
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+ 
+# Deleting popups
+ 
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+ 
+ 
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+ 
+ 
+# Designing Main(first) window
+ 
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+    main_screen.geometry("300x250")
+    main_screen.title("Account Login")
+    Label(text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Button(text="Login", height="2", width="30", command = login).pack()
+    Label(text="").pack()
+    Button(text="Register", height="2", width="30", command=register).pack()
+ 
+    main_screen.mainloop()
+
+main_account_screen()
+
+
+# # Create an instance of tkinter window
+# win = Tk()
+
+# # Define the geometry of the window
+# win.geometry("700x500")
+
+# frame = Frame(win, width=600, height=400)
+# frame.pack()
+# frame.place(anchor='center', relx=0.5, rely=0.5)
+
+# # Create an object of tkinter ImageTk
+# img = ImageTk.PhotoImage(Image.open("forest.jpg"))
+
+# # Create a Label Widget to display the text or Image
+# label = Label(frame, image = img)
+# label.pack()
+
+# win.mainloop()
+   
