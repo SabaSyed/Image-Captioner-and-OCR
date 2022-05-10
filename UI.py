86219a75aa31315169e60564b13a5e7d253749e7
SabaSyed <68774325+SabaSyed@users.noreply.github.com>
2022-05-10 20:59:09 +0500

    GUI Modules

--- UI.py
+++ UI.py
@@ -1,208 +1,314 @@
+from cProfile import label
+from distutils.command.config import config
 from tkinter import *
-import os
-import cv2
-import numpy as np
-import glob
-import random
-from PIL import ImageTk,Image
-from tkinter.filedialog import askopenfile
+import tkinter as tk
+from tkinter import ttk, messagebox
+from unittest import result
+import pymysql
 from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from PIL import Image, ImageTk
+from googletrans import Translator
+from gtts import gTTS
+from playsound import playsound
+from tkinter.messagebox import showinfo
+
+#---------------------------------------------------------------Login Function --------------------------------------
+def clear():
+    userentry.delete(0,END)
+    passentry.delete(0,END)
+
+def close():
+    win.destroy()	
 
 
-# Designing window for registration
- 
-def register():
-    global register_screen
-    register_screen = Toplevel(main_screen)
-    register_screen.title("Register")
-    register_screen.geometry("300x250")
- 
-    global username
-    global password
-    global username_entry
-    global password_entry
-    username = StringVar()
-    password = StringVar()
- 
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
-    Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
-    username_lable.pack()
-    username_entry = Entry(register_screen, textvariable=username)
-    username_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
-    password_lable.pack()
-    password_entry = Entry(register_screen, textvariable=password, show='*')
-    password_entry.pack()
-    Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
- 
- 
-# Designing window for login 
- 
 def login():
-    global login_screen
-    login_screen = Toplevel(main_screen)
-    login_screen.title("Login")
-    login_screen.geometry("300x250")
-    Label(login_screen, text="Please enter details below to login").pack()
-    Label(login_screen, text="").pack()
- 
-    global username_verify
-    global password_verify
- 
-    username_verify = StringVar()
-    password_verify = StringVar()
- 
-    global username_login_entry
-    global password_login_entry
- 
-    Label(login_screen, text="Username * ").pack()
-    username_login_entry = Entry(login_screen, textvariable=username_verify)
-    username_login_entry.pack()
-    Label(login_screen, text="").pack()
-    Label(login_screen, text="Password * ").pack()
-    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
-    password_login_entry.pack()
-    Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
- 
-# Implementing event on register button
- 
-def register_user():
- 
-    username_info = username.get()
-    password_info = password.get()
- 
-    file = open(username_info, "w")
-    file.write(username_info + "\n")
-    file.write(password_info)
-    file.close()
- 
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
- 
-    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
- 
-# Implementing event on login button 
- 
-def login_verify():
-    username1 = username_verify.get()
-    password1 = password_verify.get()
-    username_login_entry.delete(0, END)
-    password_login_entry.delete(0, END)
- 
-    list_of_files = os.listdir()
-    if username1 in list_of_files:
-        file1 = open(username1, "r")
-        verify = file1.read().splitlines()
-        if password1 in verify:
-            login_sucess()
- 
-        else:
-            password_not_recognised()
- 
+    if user_name.get()=="" or password.get()=="":
+        messagebox.showerror("Error","Enter User Name And Password",parent=win)	
     else:
-        user_not_found()
- 
-# Designing popup for login success
- 
-def login_sucess():
-    login_screen.destroy()
-    main_screen.destroy()
-    global my_w 
-    my_w= Tk()
-    my_w.geometry("400x300")  # Size of the window 
-    my_w.title('www.plus2net.com')
-    my_font1=('times', 18, 'bold')
-    l1 = Label(my_w,text='Add Student Data with Photo',width=30,font=my_font1)  
-    l1.grid(row=1,column=1)
-    b1 = Button(my_w, text='Upload File', 
-    width=20,command = lambda:upload_file())
-    b1.grid(row=2,column=1) 
+        try:
+            con = pymysql.connect(host="localhost",user="root",password="",database="user_fyp")
+            cur = con.cursor()
 
- 
-def upload_file():
-    global img
-    f_types = [('Jpg Files', '*.jpg')]
-    filename = filedialog.askopenfilename(filetypes=f_types)
-    # img = ImageTk.PhotoImage(file=filename)
-    # img_resized=img.resize((400,200)) # new width & height
-    # img=ImageTk.PhotoImage(img_resized)
+            cur.execute("select * from User_login where username=%s and pass = %s",(user_name.get(),password.get()))
+            row = cur.fetchone()
 
-    img=Image.open(filename)
-    width, height = img.size
-    width_new=int(width/3)
-    height_new=int(height/3)
-    img_resized=img.resize((width_new,height_new))
+            if row==None:
+                messagebox.showerror("Error" , "Invalid User Name And Password", parent = win)
 
-    b2 =Button(my_w,image=img) # using Button 
-    b2.grid(row=3,column=1)
+            else:
+                messagebox.showinfo("Success" , "Successfully Login" , parent = win)
+                close()
+                deshboard()
+            con.close()
+        except Exception as es:
+            messagebox.showerror("Error" , f"Error Dui to : {str(es)}", parent = win)
 
-# Designing popup for login invalid password
- 
-def password_not_recognised():
-    global password_not_recog_screen
-    password_not_recog_screen = Toplevel(login_screen)
-    password_not_recog_screen.title("Success")
-    password_not_recog_screen.geometry("150x100")
-    Label(password_not_recog_screen, text="Invalid Password ").pack()
-    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
- 
-# Designing popup for user not found
- 
-def user_not_found():
-    global user_not_found_screen
-    user_not_found_screen = Toplevel(login_screen)
-    user_not_found_screen.title("Success")
-    user_not_found_screen.geometry("150x100")
-    Label(user_not_found_screen, text="User Not Found").pack()
-    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
- 
-# Deleting popups
- 
-def delete_password_not_recognised():
-    password_not_recog_screen.destroy()
- 
- 
-def delete_user_not_found_screen():
-    user_not_found_screen.destroy()
- 
- 
-# Designing Main(first) window
- 
-def main_account_screen():
-    global main_screen
-    main_screen = Tk()
-    main_screen.geometry("300x250")
-    main_screen.title("Account Login")
-    Label(text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
-    Label(text="").pack()
-    Button(text="Login", height="2", width="30", command = login).pack()
-    Label(text="").pack()
-    Button(text="Register", height="2", width="30", command=register).pack()
- 
-    main_screen.mainloop()
+#---------------------------------------------------------------End Login Function ---------------------------------
 
-main_account_screen()
+#---------------------------------------------------- DeshBoard Panel -----------------------------------------
+def deshboard():
+    global lang, result
+    translator = Translator()
+
+    def img_insert():
+        global img
+        f_types = [('Jpg Files', '*.jpg')]
+        filename = filedialog.askopenfilename(filetypes=f_types)
+        img=Image.open(filename)
+        img_resized=img.resize((350,350)) # new width & height
+        img=ImageTk.PhotoImage(img_resized)
+        label=Label(des,image=img)
+        label.place(x=80,y=75)    
+        
+
+    des = Tk()
+    des.title("Home BeMyVision")	
+    des.maxsize(width=1000 ,  height=500)
+    des.minsize(width=1000 ,  height=500)	
+
+        #heading label
+    heading = Label(des , text = f"    Username :  {user_name.get()}    " , font = 'Verdana 20 bold',bg='#373737', fg='#ffffff')
+    heading.place(x=270 , y=30)
+
+    f=Frame(des,height=350,width=350,bg="#FFF")
+    f.place(x=80,y=75)
+    btn_insert= Button(des, text = "Insert Image" ,font='Verdana 10 bold', width = 20, command = img_insert)
+    btn_insert.place(x=150, y=440)
 
 
-# # Create an instance of tkinter window
-# win = Tk()
+    # Book Docter Appointment App
+    heading = Label(des , text = "Caption: " , font = 'Verdana 20 bold')
+    heading.place(x=450 , y=100)	
 
-# # Define the geometry of the window
-# win.geometry("700x500")
 
-# frame = Frame(win, width=600, height=400)
-# frame.pack()
-# frame.place(anchor='center', relx=0.5, rely=0.5)
+    # lang_ls = Label(des, text= "Select Language:" , font='Verdana 10 bold')
+    # lang_ls.place(x=480,y=185)
 
-# # Create an object of tkinter ImageTk
-# img = ImageTk.PhotoImage(Image.open("forest.jpg"))
+    # Dropdown menu options
+    options = {
+        "English":"en",
+        "Urdu":"ur",
+        "French":"fr",
+        "Spanish":"es",
+        "Korean":"ko",
+        "Arabic":"ar",
+        "Italian":"it"
+    }
+    cap='Hello, I am a girl.'
+    label = ttk.Label(text="Select a language:", font=("Arial", 13))
+    label.place(x=450,y=185)
 
-# # Create a Label Widget to display the text or Image
-# label = Label(frame, image = img)
-# label.pack()
+    # create a combobox
+    selected = tk.StringVar()
+    lang_cb = ttk.Combobox(des, textvariable=selected)
 
-# win.mainloop()
+    # get languages name
+    lang_cb['values'] = ("English","Urdu","French","Spanish","Korean","Arabic","Italian")
+
+    # prevent typing a value
+    lang_cb['state'] = 'readonly'
+
+    # place the widget
+    lang_cb.place(x=600,y=188)
+
+    label2 = ttk.Label(text='Caption:',width=20, font=("Arial", 13))
+    label2.place(x=450,y=218)
+    label3 = ttk.Label(text=cap,width=70, font=("Arial", 12))
+    label3.place(x=600,y=218)
    
+    def translatelang(event):
+        lang=options[selected.get()]
+        result = translator.translate(cap, dest=lang)
+        label3.config(text=result.text)             
+        # cap.config(text=result)        
+    lang_cb.bind('<<ComboboxSelected>>', translatelang)
+
+    def audio():
+        lang=options[selected.get()]
+        myobj = gTTS(text=label3.cget("text"), lang=lang, slow=False)
+        myobj.save("caption.mp3")
+        playsound('caption.mp3')
+
+    btn_audio = ttk.Button(text='Audio', command=audio)
+    btn_audio.place(x=450,y=248)   
+
+
+
+
+    
+                    
+#-----------------------------------------------------End Deshboard Panel -------------------------------------
+#----------------------------------------------------------- Signup Window --------------------------------------------------
+
+def signup():
+    # signup database connect 
+    def action():
+        if first_name.get()=="" or last_name.get()=="" or user_name.get()=="" or password.get()=="" or very_pass.get()=="":
+            messagebox.showerror("Error" , "All Fields Are Required" , parent = winsignup)
+        elif password.get() != very_pass.get():
+            messagebox.showerror("Error!" , "Password & Confirm Password Should Be Same." , parent = winsignup)
+        else:
+            try:
+                con = pymysql.connect(host="localhost",user="root",password="",database="user_fyp")
+                cur = con.cursor()
+                cur.execute("select * from User_login where username=%s",user_name.get())
+                row = cur.fetchone()
+                if row!=None:
+                    messagebox.showerror("Error" , "User Name Already Exits", parent = winsignup)
+                else:
+                    cur.execute("insert into User_login(f_name,l_name,username,pass) values(%s,%s,%s,%s)",
+                        (
+                        first_name.get(),
+                        last_name.get(),
+                        user_name.get(),
+                        password.get()
+                        ))
+                    con.commit()
+                    con.close()
+                    messagebox.showinfo("Success" , "Registration Successfull!!" , parent = winsignup)
+                    clear()
+                    switch()
+                
+            except Exception as es:
+                messagebox.showerror("Error!" , f"Error Due to : {str(es)}", parent = winsignup)
+
+    # close signup function			
+    def switch():
+        winsignup.destroy()
+
+    # clear data function
+    def clear():
+        first_name.delete(0,END)
+        last_name.delete(0,END)
+        user_name.delete(0,END)
+        password.delete(0,END)
+        very_pass.delete(0,END)
+
+
+    # start Signup Window	
+    winsignup = Tk()
+    winsignup.title("BeMyVIsion")
+    winsignup.maxsize(width=600 ,  height=400)
+    winsignup.minsize(width=600 ,  height=400)
+
+
+    #heading label
+    heading = Label(winsignup , text = "Signup" , font = 'Verdana 20 bold')
+    heading.place(x=80 , y=60)
+
+    # form data label
+    first_name = Label(winsignup, text= "First Name :" , font='Verdana 10 bold')
+    first_name.place(x=80,y=130)
+
+    last_name = Label(winsignup, text= "Last Name :" , font='Verdana 10 bold')
+    last_name.place(x=80,y=160)
+
+    user_name = Label(winsignup, text= "Username :" , font='Verdana 10 bold')
+    user_name.place(x=80,y=190)
+
+    password = Label(winsignup, text= "Password :" , font='Verdana 10 bold')
+    password.place(x=80,y=220)
+
+    very_pass = Label(winsignup, text= "Verify Password:" , font='Verdana 10 bold')
+    very_pass.place(x=80,y=260)
+
+    # Entry Box ------------------------------------------------------------------
+
+    first_name = StringVar()
+    last_name = StringVar()
+    user_name = StringVar()
+    password = StringVar()
+    very_pass = StringVar()
+
+
+    first_name = Entry(winsignup, width=30 , textvariable = first_name)
+    first_name.place(x=230 , y=133)
+
+
+    last_name = Entry(winsignup, width=30 , textvariable = last_name)
+    last_name.place(x=230 , y=163)
+
+    
+    user_name = Entry(winsignup, width=30,textvariable = user_name)
+    user_name.place(x=230 , y=193)
+
+    
+    password = Entry(winsignup, width=30,show="*", textvariable = password)
+    password.place(x=230 , y=223)
+
+    
+    very_pass= Entry(winsignup, width=30 ,show="*" , textvariable = very_pass)
+    very_pass.place(x=230 , y=253)
+
+    # button login and clear
+
+    btn_signup = Button(winsignup, text = "Register" ,font='Verdana 10 bold', command = action)
+    btn_signup.place(x=230, y=283)
+
+
+    btn_login = Button(winsignup, text = "Clear" ,font='Verdana 10 bold' , command = clear)
+    btn_login.place(x=360, y=283)
+
+
+    sign_up_btn = Button(winsignup , text="Already have an account?" , command = switch )
+    sign_up_btn.place(x=350 , y =20)
+
+
+    winsignup.mainloop()
+#---------------------------------------------------------------------------End Singup Window-----------------------------------	
+
+
+    
+
+#------------------------------------------------------------ Login Window -----------------------------------------
+
+win = Tk()
+
+# app title
+win.title("BeMyVision")
+
+# window size
+win.maxsize(width=600 ,  height=400)
+win.minsize(width=600 ,  height=400)
+
+
+#heading label
+heading = Label(win , text = "Login" , font = 'Verdana 25 bold')
+heading.place(x=80 , y=150)
+
+username = Label(win, text= "User Name :" , font='Verdana 10 bold')
+username.place(x=80,y=220)
+
+userpass = Label(win, text= "Password :" , font='Verdana 10 bold')
+userpass.place(x=80,y=260)
+
+# Entry Box
+user_name = StringVar()
+password = StringVar()
+    
+userentry = Entry(win, width=30 , textvariable = user_name)
+userentry.focus()
+userentry.place(x=200 , y=223)
+
+passentry = Entry(win, width=30, show="*" ,textvariable = password)
+passentry.place(x=200 , y=260)
+
+
+# button login and clear
+
+btn_login = Button(win, text = "Login" ,font='Verdana 10 bold',command = login)
+btn_login.place(x=200, y=293)
+
+
+btn_login = Button(win, text = "Clear" ,font='Verdana 10 bold', command = clear)
+btn_login.place(x=300, y=293)
+
+# signup button
+
+sign_up_btn = Button(win , text="Register new account " , command = signup )
+sign_up_btn.place(x=350 , y =20)
+
+
+
+win.mainloop()
